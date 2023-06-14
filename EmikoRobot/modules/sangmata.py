584bb313f53f7ed3c42ae38698ac22ad797ab220
Geasaputra <108199249+Geasaputra@users.noreply.github.com>
2023-06-14 23:01:41 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -21,7 +21,7 @@ async def sg(client, message):
     user_id = message.sender.id
     id =f"/search_id {user_id}"
     
-     if message.sender.bot:
+     reply_text:
         await lol.edit(bot, f"{user.id}")
 
     except YouBlockedUser:
