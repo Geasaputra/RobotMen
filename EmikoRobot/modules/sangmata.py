5e8acfff1dbbd267b46dcc5ea88e832c1f359d7f
Geasaputra <108199249+Geasaputra@users.noreply.github.com>
2023-06-14 21:53:34 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -1,40 +1,23 @@
-from pyrogram import filters
-
 from pyrogram.errors import YouBlockedUser
-
 from pyrogram.raw.functions.messages import DeleteHistory
 from EmikoRobot import OWNER_ID
-from EmikoRobot import pbot as Client
-from EmikoRobot import telethn as tbot
-
+from EmikoRobot import pbot
 from EmikoRobot import ubot2 as ubot
-
 from asyncio.exceptions import TimeoutError
 
 
 
-
-@Client.on_message(filters.command("sg"))
+@pbot.on_message(filters.command("sg"))
 async def sg(client, message):
-
     response = await extract_user(message)
-
     lol = await eor(message, "Sedang Memproses...")
-
     if args:
-
-        try:
-
-            user = await client.get_users(args)
-
-        except Exception as error:
-
-            return await lol.edit(error)
-
-    bot = "@SangMata_BOT"
-
-    try:
-
+       try:
+          user = await client.get_users(args)
+       except Exception as error:
+           return await lol.edit(error)
+     bot = "@SangMata_BOT"      
+     try:
         txt = await client.send_message(bot, f"{user.id}")
 
     except YouBlockedUser:
@@ -62,5 +45,16 @@ async def sg(client, message):
             await message.reply(stalk.text)
 
     user_info = await client.resolve_peer(bot)
+         
+        
+        
 
+        
 
+            
+
+    
+
+    
+
+        
