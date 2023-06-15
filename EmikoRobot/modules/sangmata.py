0ade12dff3472d1f65c1cc4a2cf672863519a47e
Geasaputra <108199249+Geasaputra@users.noreply.github.com>
2023-06-15 11:32:42 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -21,45 +21,4 @@ async def sg(client, message):
     user_id = message.sender.id
     id =f"/search_id {user_id}"
     
-      await lol.edit(bot, f"{user.id}")
-
-except YouBlockedUser:
-
-        await client.unblock_user(bot)
-
-        await client.send_message(bot, f"{user.id}")
-
-    await txt.delete()
-    await asyncio.sleep(5)
-    await lol.delete()
-
-    
-async for stalk in client.search_messages(bot, query="History", limit=1):
-     if not stalk
-            NotFound = await client.send_message(client.me.id, "Tidak ada komentar")
-
-            await NotFound.delete()
-
-        elif stalk:
-
-            await message.reply(stalk.text)
-
-    user_info = await client.resolve_peer(bot)
-    
-    
-
-    
-        
-         
-        
-        
-
-        
-
-            
-
-    
-
-    
-
-        
+      
